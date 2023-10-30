9f7f09d13d3e980d364b8517b64858ebe5871071
Alperen Emre Candan <91540339+alperenec@users.noreply.github.com>
2023-10-31 00:07:06 +0300

    Add files via upload

---
+++ 3_text functions/text functions.sql
@@ -0,0 +1,28 @@
+--METİNSEL FONKSİYONLAR
+
+select * from bolum2
+
+select ascii ('A')
+
+select concat('gunaydın ','SQL ','dersleri')
+
+select concat_ws('^^','gunaydın ','SQL ','dersleri')
+
+select left('Merhaba Dunya',3)
+
+select right('Merhaba Dunya',6)
+
+select length('Benim manevi mirasım bilim ve akıldır')
+
+select id,adi from bolum3
+select id,replace(adi,'i','ı') from bolum3
+
+select reverse(adi) from bolum3
+
+select substring('Alperen Candan',4,2)       --4. karakterden sonra 2 değer
+
+select lower(adi), upper(adi) from bolum2
+
+
+
+
